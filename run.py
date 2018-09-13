5ab303d802fb47779ff69305998af04ca19145eb
neo <chiyuduanhun@126.com>
2018-09-13 22:29:00 +0800

    add predefined comments

--- run.py
+++ run.py
@@ -1,1 +1,21 @@
 #! /usr/bin/env python3
+"""
+The run shell for the whole tool, which is a user accessible command used to call all other sub commands.
+---------------------------------------------------------------------------------------------------------
+Sub commands this tool supports:
+
+---------------------------------------------------------------------------------------------------------
+The interfaces we should defined here:
+
+
+
+"""
+
+
+## -- Global Vars ---------------------------------------------------------------------------------------
+## this block will define all global variables that may be used in the whole module, or other external
+## code.
+## ------------------------------------------------------------------------------------------------------
+
+
+## ------------------------------------------------------------------------------------------------------
